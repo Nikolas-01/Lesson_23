059d8f430ffc06904715b6628c554aa896584ebf
ПК <89068414983@bk.ru>
2020-11-08 21:18:20 +0500

    11

--- users/urls.py
+++ users/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
 
@@ -22,7 +21,7 @@ from django.contrib.auth.views import LogoutView
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.UserLoginView.as_view(), name = 'login'),
+    path('login/', views.UserLoginView.as_view(), name ='login'),
     path('register/', views.UserCreateView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('token_options/', views.get_token, name='token_options'),
